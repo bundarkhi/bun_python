1c96d96ab8c34d9555614192dd10999d7b9f53df
bundarkhi bat-ulzii <buna0121@gmail.com>
2022-06-05 11:15:48 +0800

    test hiij uzew

---
+++ from decimal import ROUND_DOWN.py
@@ -0,0 +1,19 @@
+from decimal import ROUND_DOWN
+
+
+5+6 #add
+5*6 #multipy
+5/6 #divide
+5**2 #^
+5**3 #power
+3**(4/2) #power
+print("bun")
+max(4, 7, 8, 910, 74, 158)
+round(3.7)
+
+num1 = input("enter a number:")
+num2 = input("enter another number:")
+result = num1 + num2
+print(result)
+
+
